d2863a3154cbe6588b730ab2d31ed48fa18381b3
Shado5 <107094799+Shado5@users.noreply.github.com>
2022-08-22 20:47:37 +1000

    Chest

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FishMovement : MonoBehaviour
 {
     public float speed = 2f;
+    public ParticleSystem ParticleSystem;
     void Start()
     {
-        
+        ParticleSystem.Stop();
     }
 
    
@@ -16,18 +17,23 @@ public class FishMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
+            ParticleSystem.Play();
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
+            ParticleSystem.Play();
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            ParticleSystem.Play();
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            ParticleSystem.Play();
         }
+        
     }
 }
